c4ca718970584b913207567b16b0459d27c6527d
Станислав Грибанов <youkillbird@gmail.com>
2023-03-07 15:31:15 +0500

    Unworking built for commitor's review

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,14 +7,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 
-test('compare two file, format: JSON', () => {
-  const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expectedDiff = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const plainDiff = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const nestedDiff = '{\n  common: {\n    + follow: false\n      setting1: Value 1\n    - setting2: 200\n    - setting3: true\n    + setting3: null\n    + setting4: blah blah\n    + setting5: {\n          key5: value5\n      }\n      setting6: {\n          doge: {\n            - wow:\n            + wow: so much\n          }\n          key: value\n        + ops: vops\n      }\n  }\n  group1: {\n    - baz: bas\n    + baz: bars\n      foo: bar\n    - nest: {\n          key: value\n      }\n    + nest: str\n  }\n- group2: {\n      abc: 12345\n      deep: {\n          id: 45\n      }\n  }\n+ group3: {\n      deep: {\n          id: {\n              number: 45\n          }\n      }\n      fee: 100500\n  }\n};';
+
+test('compare two file, type: plain, format: JSON', () => {
+  const diff = genDiff(getFixturePath('plain/file1.json'), getFixturePath('plain/file2.json'));
+  const expectedDiff = plainDiff;
   expect(diff).toContain(expectedDiff);
 });
 
-test('compare two file, format: YAML', () => {
-  const diff = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
-  const expectedDiff = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+test('compare two file, type: plain, format: YAML', () => {
+  const diff = genDiff(getFixturePath('plain/file1.yml'), getFixturePath('plain/file2.yml'));
+  const expectedDiff = plainDiff;
+  expect(diff).toContain(expectedDiff);
+});
+
+test('compare two file, type: nested, format: JSON', () => {
+  const diff = genDiff(getFixturePath('nested/file1.json'), getFixturePath('nested/file2.json'));
+  const expectedDiff = nestedDiff;
   expect(diff).toContain(expectedDiff);
 });
--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,61 @@ const getDiff = (obj1, obj2) => {
   const commonKeys = _.union(Object.keys(obj1), Object.keys(obj2));
   const keys = _.uniq(commonKeys).sort();
 
+  const diff = keys.reduce((acc, key) => {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      const objDiff = getDiff(obj1[key], obj2[key]);
+      acc.push(`${key}:{\n${objDiff}\n}`);
+      return acc;
+    }
+
+    if (_.isObject(obj1[key]) && !_.isObject(obj2[key])) {
+      const objToStr = obj1[key].toString();
+      acc.push(`${key}:{\n${objToStr}\n}`);
+      return acc;
+    }
+
+    if (!_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      const objToStr = obj2[key].toString();
+      acc.push(`${key}:{\n${objToStr}\n}`);
+      return acc;
+    }
+
+    const keyValueObj1 = `${key}: ${obj1[key]}`;
+    const keyValueObj2 = `${key}: ${obj2[key]}`;
+    if (!Object.hasOwn(obj1, key)) {
+      acc.push(`  + ${keyValueObj2}`);
+      return acc;
+    }
+    if (!Object.hasOwn(obj2, key)) {
+      acc.push(`  - ${keyValueObj1}`);
+      return acc;
+    }
+    if (obj1[key] === obj2[key]) {
+      acc.push(`    ${keyValueObj1}`);
+      return acc;
+    }
+    acc.push(`  - ${keyValueObj1}`);
+    acc.push(`  + ${keyValueObj2}`);
+    return acc;
+  }, []);
+  return diff.join('\n');
+};
+
+const generateStr = (diff, filepath1, filepath2) => {
+  const str = `gendiff ${filepath1} ${filepath2}\n{\n${diff}\n}`;
+  return str;
+};
+
+export {
+  getDiff,
+  generateStr,
+};
+
+/*
+const getDiff = (obj1, obj2) => {
+  const commonKeys = _.union(Object.keys(obj1), Object.keys(obj2));
+  const keys = _.uniq(commonKeys).sort();
+
   const result = keys.reduce((acc, key) => {
     const keyValueObj1 = `${key}: ${obj1[key]}`;
     const keyValueObj2 = `${key}: ${obj2[key]}`;
@@ -25,13 +80,4 @@ const getDiff = (obj1, obj2) => {
   }, []);
   return result.join('\n');
 };
-
-const generateStr = (diff, filepath1, filepath2) => {
-  const str = `gendiff ${filepath1} ${filepath2}\n{\n${diff}\n}`;
-  return str;
-};
-
-export {
-  getDiff,
-  generateStr,
-};
+*/
