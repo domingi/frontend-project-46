ebbc2cb910410dd65095246e8fbbfdb0b9cb0598
Станислав Грибанов <youkillbird@gmail.com>
2023-03-30 20:54:23 +0500

    Optimize formatter stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,37 +6,22 @@ import {
 
 const getSpaces = (depth) => (_.repeat(' ', depth * 4));
 
-const getValue = (node, type, depth) => {
-  const takeValue = (node, type) => {
-    switch (type) {
-      case 'common':
-        return node.value;
-      case 'new':
-        return node.newValue;
-      case 'old':
-        return node.oldValue;
-      default:
-        throw new Error(`Undefined type of value ${type}`);
-    }
-  };
-
-  const value = takeValue(node, type);
-
+const getValue = (value, depth) => {
   const valueToStr = (value, depth) => {
     const entries = Object.entries(value);
-    const prettyValue = entries.map(([key, val]) => {
+    const str = entries.map(([key, val]) => {
       if (_.isPlainObject(val)) {
         return `${getSpaces(depth)}${key}: {\n${valueToStr(val, depth + 1)}\n${getSpaces(depth)}}`;
       }
       return `${getSpaces(depth)}${key}: ${val}`;
     });
-    return prettyValue.join('\n');
+    return str.join('\n');
   };
 
   return _.isPlainObject(value) ? `{\n${valueToStr(value, depth + 1)}\n${getSpaces(depth)}}` : value;
 };
 
-const getIndent = (node, depth, type = 'unchanged') => {
+const getIndent = (depth, type = 'unchanged') => {
   const indent = _.repeat(' ', depth * 4 - 2);
   switch (type) {
     case 'added':
@@ -55,23 +40,23 @@ const makeStylish = (diffTree) => {
     const result = data.map((node) => {
       if (getChildren(node) !== null) {
         const expression = iter(getChildren(node), depth + 1);
-        return `${getIndent(node, depth)}${getKey(node)}: {\n${expression}\n${getIndent(node, depth)}}`;
+        return `${getIndent(depth)}${getKey(node)}: {\n${expression}\n${getIndent(depth)}}`;
       }
 
       if (getType(node) === 'removed') {
-        return `${getIndent(node, depth, 'removed')}${getKey(node)}: ${getValue(node, 'common', depth)}`;
+        return `${getIndent(depth, 'removed')}${getKey(node)}: ${getValue(node.value, depth)}`;
       }
 
       if (getType(node) === 'added') {
-        return `${getIndent(node, depth, 'added')}${getKey(node)}: ${getValue(node, 'common', depth)}`;
+        return `${getIndent(depth, 'added')}${getKey(node)}: ${getValue(node.value, depth)}`;
       }
 
       if (getType(node) === 'modified') {
-        return `${getIndent(node, depth, 'removed')}${getKey(node)}: ${getValue(node, 'old', depth)}\n${getIndent(node, depth, 'added')}${getKey(node)}: ${getValue(node, 'new', depth)}`;
+        return `${getIndent(depth, 'removed')}${getKey(node)}: ${getValue(node.oldValue, depth)}\n${getIndent(depth, 'added')}${getKey(node)}: ${getValue(node.newValue, depth)}`;
       }
 
       if (getType(node) === 'unchanged') {
-        return `${getIndent(node, depth)}${getKey(node)}: ${getValue(node, 'common', depth)}`;
+        return `${getIndent(depth)}${getKey(node)}: ${getValue(node.value, depth)}`;
       }
       return node;
     });
